911edf0ac7299e7ffb42a0ccf7cc27bcdd37305a
Kaydrax <jguiheneuf44@gmail.com>
2018-09-02 17:13:09 +0200

    Added concern module for placholder images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,7 @@
 class Portfolio < ApplicationRecord
+
+	include Placeholder
+
 	validates_presence_of :title, :main_image, :thumb_image
 
 	scope :angular, -> { where(subtitle: "Angular") }
@@ -6,7 +9,7 @@ class Portfolio < ApplicationRecord
 	after_initialize :set_defaults
 
 	def set_defaults
-		self.main_image ||="https://via.placeholder.com/650x350"
-		self.thumb_image ||="https://via.placeholder.com/350x200"
+		self.main_image ||=Placeholder.image_generator("650","350")
+		self.thumb_image ||=Placeholder.image_generator("350","200")
 	end
 end
