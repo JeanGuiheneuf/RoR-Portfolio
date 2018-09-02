48ec56c4e2a52f3f68dc196edf11f538f122bf48
Kaydrax <jguiheneuf44@gmail.com>
2018-09-02 16:19:56 +0200

    Added by default images by setting default values

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,11 @@ class Portfolio < ApplicationRecord
 	validates_presence_of :title, :main_image, :thumb_image
 
 	scope :angular, -> { where(subtitle: "Angular") }
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||="https://via.placeholder.com/650x350"
+		self.thumb_image ||="https://via.placeholder.com/350x200"
+	end
 end
