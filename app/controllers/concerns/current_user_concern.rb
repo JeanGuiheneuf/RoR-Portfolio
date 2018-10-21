120f467e14fdba82758e606df6ac29db7fded28c
Kaydrax <jguiheneuf44@gmail.com>
2018-10-22 00:45:47 +0200

    Implemented code for authorization

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -8,10 +8,13 @@ module CurrentUserConcern
 	
 
 	def guest_user
-		OpenStruct.new(name: "Guest User",
-						 first_name: "Guest",
-						 last_name:"User",
-						 email: "abc@guest.com"
-						 )
+		guest = GuestUser.new
+		guest.name = "Guest User"
+		guest.first_name = "Guest"
+		guest.last_name = "User"
+		guest.email = "guest@guest.com"
+		guest
+
+
 	end
 end
