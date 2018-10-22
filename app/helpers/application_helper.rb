03fb637e58da7319a94ab237c3690e09faa38960
Kaydrax <jguiheneuf44@gmail.com>
2018-10-22 23:39:18 +0200

    Updated master layout file along with helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
-	def login_helper
+	def login_helper style
 		if current_user.is_a?(GuestUser)
-	      (link_to 'log in', new_user_session_path)+
-	      "<br>".html_safe +
-	      (link_to 'sign up', new_user_registration_path) 
+	      (link_to 'log in', new_user_session_path, class: style)+
+	      " ".html_safe + 
+	      (link_to 'sign up', new_user_registration_path, class: style) 
 	    else
-	      (link_to 'logout', destroy_user_session_path, method: :delete)+
-	      "<br>".html_safe +
-	      (link_to 'edit profile',edit_user_registration_path)
+	      (link_to 'logout', destroy_user_session_path, method: :delete, class: style)+
+	      " ".html_safe + 
+	      (link_to 'edit profile',edit_user_registration_path, class: style)
 	    end 
 
 	end
